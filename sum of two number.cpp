39b0dc195ac8241d1db9e1df8d71c4dabf18d51b
Deepakpd811 <deepak0413.be21@chitkara.edu.in>
2022-03-21 21:05:50 +0530

    adding of new file

---
+++ sum of two number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+
+    cout << "Enter num1 number" << endl;
+    cin >> num1;
+
+    cout << "Enter num2 number" << endl;
+    cin >> num2;
+
+    sum = num1 + num2;
+
+    cout << "sum of a number is "  << sum << endl;
+
+
+
+    return 0;
+}
